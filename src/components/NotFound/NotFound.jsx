13c3886b482de1855fefe0d096c765bfd649e00b
Ilvina Rakhimova <ilvrakh@gmail.com>
2022-12-19 14:44:13 +0500

    page 404

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,9 +1,16 @@
 import style from './notFound.module.scss';
+import {Link} from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div className={style.container}>NotFound
+    <div className={style.container}>
+      <p>Even our greatest detective cannot find the page you are looking for</p>
+      <img src="images/404.gif" alt="404"/>
+      <div>But you can always 
+          <Link to = "/"> go home</Link>
+      </div>
+      
     </div>
-    
+
   )
 }
