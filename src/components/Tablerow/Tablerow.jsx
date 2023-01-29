69c209ead6e3f10523762867786e95e11b0f0a54
Ilvina Rakhimova <ilvrakh@gmail.com>
2023-01-29 22:51:33 +0500

    tablerow validation

--- src/components/Tablerow/Tablerow.jsx
+++ src/components/Tablerow/Tablerow.jsx
@@ -1,19 +1,33 @@
 import { useState, useEffect } from 'react';
 import style from './tablerow.module.scss';
 import { EditFilled, DeleteFilled } from '@ant-design/icons';
+import classNames from 'classnames';
 
 export default function Tablerow(props) {
     const {english, transcription, russian, tag} = props;
 
     const [editing, setEditing] = useState();
-    const [state, setState] = useState('');
 
-    useEffect (() => {
-        setState(props)
-    },[props])
+    const [state, setState] = useState({
+        english: english,
+        transcription: transcription,
+        russian: russian,
+        tag: tag
+    });
 
-    const handleEdit = () => {
-        setEditing(!editing)
+    const [errors, setErrors] = useState({
+        english: false,
+        transcription: false,
+        russian: false,
+        tag: false
+    });
+
+    // useEffect (() => {
+    //     setState(props)
+    // },[props])
+
+    const handleEdit = (e) => {
+        setEditing(!editing);
     }
 
     const handleCancel = () => {
@@ -21,24 +35,63 @@ export default function Tablerow(props) {
             ...props,
         });
         handleEdit();
+        setErrors(false)
     }
 
     const handleChangeInput = (event) => {
+        if (event.target.dataset.name==='english' && event.target.value.match(/[а-яА-Я]/g)) {
+            alert("Please use English letters only");
+        }
+
+        if (event.target.dataset.name==='russian' && event.target.value.match(/[a-zA-Z]/g)) {
+            alert("Please use Cyrillic alphabet only");
+        }
+
+        setErrors({
+            ...errors,
+            [event.target.dataset.name]: false,
+        })
         setState({
             ...state,
             [event.target.dataset.name]: event.target.value,
         });
+
+        validate(event.target);
     }
+
+    const validate = (input) => {
+        if (input.value === "") {
+            throwError(input);
+        }
+    }
+
+    const throwError = (input) => {
+        setErrors({
+            ...errors,
+            [input.dataset.name]: true,
+        })
+        // console.log(errors)
+        // console.log(input.dataset.name)
+    }
+
+    const validateFlag = !errors.english && !errors.transcription && !errors.russian && !errors.tag;
+
+    const errorInputEnglish = classNames(errors.english ? style.error : "");
+    const errorInputTranscription = classNames(errors.transcription ? style.error : "");
+    const errorInputRussian = classNames(errors.russian ? style.error : "");
+    const errorInputTag = classNames(errors.tag ? style.error : "");
 
     return (
         <div>
             <div className={style.row}>
-                <div>{state.english}</div>
-                <div>{state.transcription}</div>
-                <div>{state.russian}</div>
-                <div>{state.tag}</div>
+                <div>{errors.english ? 'Пустое поле' : state.english}</div>
+                <div>{errors.transcription ? 'Пустое поле' : state.transcription}</div>
+                <div>{errors.russian ? 'Пустое поле' : state.russian}</div>
+                <div>{errors.tag ? 'Пустое поле' : state.tag}</div>
                 <div>
+                {validateFlag &&
                     <button onClick={handleEdit} className={style.button}><EditFilled /></button>
+                }
                 </div>
                 <div>
                     <button className={style.button}><DeleteFilled/></button>
@@ -52,29 +105,39 @@ export default function Tablerow(props) {
                     data-name="english"
                     type="text" 
                     value={state.english} 
-                    onChange={handleChangeInput}/>
+                    className = {errorInputEnglish}
+                    onChange={handleChangeInput}
+                    />
 
                     <input 
                     data-name="transcription"
                     type="text" 
                     value={state.transcription}
+                    className = {errorInputTranscription}
                     onChange={handleChangeInput} />
 
                     <input 
                     data-name="russian"
                     type="text" 
                     value={state.russian}
+                    className = {errorInputRussian}
                     onChange={handleChangeInput} />
 
                     <input 
                     data-name="tag"
                     type="text" 
                     value={state.tag}
+                    className = {errorInputTag}
                     onChange={handleChangeInput} />
 
-                    <div>
-                        <button onClick={handleEdit} className={style.button_edit}>Save</button>
+                    {validateFlag
+                    ? <div>
+                    <button onClick={handleEdit} className={style.button_edit}>Save</button>
                     </div>
+                    : <div>
+                    <button className={style.button_error}>X</button>
+                    </div>}
+
                     <div>
                         <button onClick={handleCancel} className={style.button_edit}>Cancel</button>
                     </div>
